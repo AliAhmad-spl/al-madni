4d51298bab931878f0e296cc2db8245251e670d1
ali-spl <worldfun66@gmail.com>
2020-09-20 05:43:05 -0700

    product update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
 	belongs_to :one_menu
-	default_scope { order(:one_menu_id) }
+	default_scope { order("one_menu_id + id") }
 	enum product_type: {small: 0, medium: 1, large: 2} 
 end

7281705888be857eb2b9b188636159897316a8fa
Ali-crewlogix <ali.ahmed@crewlogix.com>
2021-02-22 12:10:23 -0800

    details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!, except: [:home]
+
+	def current_hotel
+		if current_user.hotels.any?
+		  current_user.hotels.first
+		else
+		  return nil
+		end
+	end
 end
